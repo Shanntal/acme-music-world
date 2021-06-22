b4d62e5c722d99d49ac3a77fefa33b7c26e2d23c
Shanntal Morel <51673250+Shanntal@users.noreply.github.com>
2021-06-21 21:43:47 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,76 @@
 import React from 'react';
 import { render } from 'react-dom';
+import axios from 'axios';
 
-render(<hr />, document.querySelector('#root'));
+
+// do work here
+const Artists = (props) => {
+    const {state} = props;
+    return (
+        <div>
+            <ul>
+            {props.map(artist => {
+                
+                <li>{artist}</li>
+                
+            })}
+            </ul>
+        </div>
+    )
+}
+
+const Albums = (props) => {
+    const {state} = props;
+    return (
+        <div>
+            <ul>
+            {props.map(album => {
+                
+                <li>{album}</li>
+                
+            })}
+            </ul>
+        </div>
+    )
+}
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            artists: [],
+            albums: [],
+            artistSelected: {},
+            albumSelected: {}
+        }
+    }
+
+    getArtists = () => {
+        this.setState({artistSelected: artist.id})
+    }
+
+    getAlbums = () => {
+        this.setState({albumSelected = album.id})
+    }
+
+    async componentDidMount() {
+        try {
+          const artists = (await axios.get('/api/artist')).data;
+          const albums = (await axios.get('/api/album')).data;
+          this.setState({ artists, albums });
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+    render() {
+        return (
+            <div>
+               <Album />
+               <Artist />
+            </div>
+        )
+    }
+}
+
+render(<App/>, document.querySelector('#root'));
